0ae0335056c6c6d9ffd02d219b25021f159ad9da
IMagurno <141076342+IMagurno@users.noreply.github.com>
2024-03-08 23:34:29 -0300

    Middleware /rooms

--- src/middleware.ts
+++ src/middleware.ts
@@ -8,5 +8,5 @@ export default function middleware(req: Request) {
 }
 
 export const config = {
-  matcher: ["/rooms", "/homepage"],
+  matcher: [], // VOLVER A ANADIR EL /rooms DESPUES DE SOLUCIONAR EL DEPLOY EN VERCEL
 };
